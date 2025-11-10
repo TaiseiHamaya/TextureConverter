4cd9c7f6e66d4a4c30cc67674fddd7e09b25cfa6
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2025-11-10 23:47:22 +0900

    MipMapの生成と圧縮を行うよう修正しました。 テスト用のファイルを削除しました。

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -14,8 +14,22 @@ void TextureConverter::load_wic_from_file(const std::filesystem::path& path) {
 }
 
 void TextureConverter::save_dds_to_file(const std::filesystem::path& path) {
-	metadata.format = DirectX::MakeSRGB(metadata.format);
 	HRESULT hr;
+	DirectX::ScratchImage mipChain;
+	hr = DirectX::GenerateMipMaps(scratchImage.GetImages(), scratchImage.GetImageCount(), scratchImage.GetMetadata(), DirectX::TEX_FILTER_DEFAULT, 0, mipChain);
+	assert(SUCCEEDED(hr));
+	scratchImage = std::move(mipChain);
+
+	// à≥èk
+	DirectX::ScratchImage compressed;
+	hr = DirectX::Compress(scratchImage.GetImages(), scratchImage.GetImageCount(), scratchImage.GetMetadata(),
+		DXGI_FORMAT_BC7_UNORM_SRGB, DirectX::TEX_COMPRESS_BC7_QUICK | DirectX::TEX_COMPRESS_SRGB_OUT | DirectX::TEX_COMPRESS_PARALLEL, 1.0f, compressed);
+	assert(SUCCEEDED(hr));
+	// ì]ëó
+	scratchImage = std::move(compressed);
+	metadata = scratchImage.GetMetadata();
+
+	metadata.format = DirectX::MakeSRGB(metadata.format);
 	hr = DirectX::SaveToDDSFile(scratchImage.GetImages(), scratchImage.GetImageCount(), metadata, DirectX::DDS_FLAGS_NONE, path.native().c_str());
 	assert(SUCCEEDED(hr));
 }
